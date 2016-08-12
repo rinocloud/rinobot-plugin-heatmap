549391fc7c14b306049864ada2f84ce22628b458
eoinmurray <amonecho1@gmail.com>
2016-08-12 13:54:08 +0200

    added package.json

--- index.py
+++ index.py
@@ -1,16 +1,30 @@
 import sys
 import os
 import numpy as np
+import warnings
 import matplotlib
 import matplotlib.pyplot as plt
 
 output_file_type = '.png'
-
 matplotlib.rcParams['savefig.dpi'] = 2 * matplotlib.rcParams['savefig.dpi']
+
+def load(fpath, skiprows=0):
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        try:
+            return np.loadtxt(fpath, skiprows=skiprows)
+        except ValueError:
+            return load(fpath, skiprows+1)
+        except StopIteration:
+            return []
 
 def main(filepath):
     filename_without_ext = os.path.splitext(filepath)[0]
-    data = np.loadtxt(filepath)
+    data = load(filepath)
+
+    if len(data) == 0:
+        print("No data numeric data could be parsed from %s" % filepath)
+        return
 
     plt.imshow(data, extent=[0, 100, 0, 1], aspect='auto')
     plt.savefig(filename_without_ext + output_file_type)
